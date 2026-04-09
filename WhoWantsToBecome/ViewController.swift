bd5605feb767b96715958d6f049fc135d304beef
Rysya <pbl6a@bk.ru>
2026-04-09 22:32:37 +0300

    Initial Commit

---
+++ WhoWantsToBecome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhoWantsToBecome
+//
+//  Created by Мария Александрова on 09.04.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
